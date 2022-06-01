eb3f7a3ae5aad47a7f05ef579b80c3695e9daae7
Melanie Sumner <melanie@hashicorp.com>
2022-06-01 11:51:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Collective Nouns for the Web
+
+It started as a joke, sort of, but then it seemed like a useful idea. This project intends to provide collective nouns for web-related terms.
+
+## Contributing
+
+If you see something missing, you can contribute in a few different ways:
+
+1. Open an issue. File an issue on this GitHub repo and let us know what you think should be added.
+2. Open a pull request. Open a pull request with the changes or additions you think should be considered.
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+  <title>Collective Nouns for the Web</title>
+</svelte:head>
+
+<header>
+  <img src="/assets/hero.png" alt="" width="500" />
+  <h1 class="content-width">Collective Nouns for the Web</h1>
+</header>
+<main>
+  <section>
+    <dl>
+      <dt>Vanity (of Domains)</dt>
+      <dd>10 or more domains owned by a single person.</dd>
+      <dt>Arsenal (of StackOverflow Answers)</dt>
+      <dd>A stackoverflow question with 6 or more popular answers.</dd>
+    </dl>
+  </section>
+</main>
+<footer>
+
+</footer>
