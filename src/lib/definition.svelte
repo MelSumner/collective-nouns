d06c9a3dd559bbecb508e517678e14f279ab7f66
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-02 08:35:24 -0400

    Creates a Definition component
    The main aim of this is to reduce the amount of HTML we're writing
    directly to the index.svelte file.
    
    Also opens the door for using the API mentioned in issue 3

---
+++ src/lib/definition.svelte
@@ -0,0 +1,10 @@
+<script>
+export let term = '';
+export let definition = '';
+
+definition = definition.replace(`${term}`, `<strong>${term.toLowerCase()}</strong>`);
+
+</script>
+<dt>{term}</dt>
+<dd>{definition}</dd>
+<style>/* Just leaving this here so it exists in the component */</style>
